267329db6042d735f8bfa8a21f1eeffd07a9023c
hsc de xolusion <tayworgbalestevestan@gmail.com>
2025-06-25 04:15:34 +0000

    made changes in the main file and added the widget tree as well a home screen

---
+++ lib/views/widget_tree.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'pages/home_screen.dart';
+
+class WidgetTree extends StatelessWidget {
+  const WidgetTree({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return HomeScreen();
+  }
+}
